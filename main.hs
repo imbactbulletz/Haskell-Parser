3941299965b1bc07a904ece9a9466f48de4bc085
Stefan <cveticstefancpl@gmail.com>
2019-07-06 23:31:17 +0200

    Implemented parsing of 2nd type.

--- main.hs
+++ main.hs
@@ -1,23 +1,14 @@
--- biblioteke za rad sa parserom i fajlovima
 import Text.Parsec
 import System.Environment
 
-
 -- gramatika
 data Document = Document [Record] deriving (Show)
-
 data Record = Record Leader [Field] deriving (Show)
-
 data Leader = Leader Code Value deriving (Show)
-
 data Field = Field Code [Indicator] [Subfield] deriving (Show)
-
 data Indicator = Indicator Char deriving (Show)
-
 data Subfield = Subfield Code Value deriving (Show)
-
 type Value = String
-
 type Code = String
 
 -- shorthand za UNIMARC nevidljive znakove
@@ -61,9 +52,14 @@ parseRecord = do
     fields <- many parseFieldType1 -- ukoliko je prvi tip, onda parsira field-ove za taj odgovarajuci tip
     char groupSeparator -- jede GS na kraju record-a da bi se mogao parsirati sledeci potencijalni record
     return (Record leader fields) -- vraca record sa leader-om i field-ovima
-  else do -- ostali slucajevi TODO
-    char groupSeparator
-    return (Record leader [])
+  else if((recordMARCType:: Int) == 2) then do -- drugi tip
+      fields <- many parseFieldType2 -- parsira polja za drugi tip
+      char groupSeparator
+      return (Record leader fields)
+    else do
+      values <- many (noneOf [groupSeparator])
+      char groupSeparator
+      return (Record leader [])
 
 
 -- funkcija za parsiranje leader-a
@@ -71,8 +67,8 @@ parseLeader :: Parsec String Int Leader
 parseLeader = do
   code <- try (string "000") <|> try (string "LEADER") <|> try (string "LDR") -- pokusamo da parsiramo jedan od tri vrste leader-a, ako ne uspe ova funkcija se zavrsava
   case code of -- switch nad vrednosti koju smo isparsirali
-    "000" -> putState 3 -- u pitanju je treci tip marc-a, stavlja ga u state parsera
-    "LEADER" -> putState 2 -- drugi tip, ditto
+    "000" -> putState 2 -- u pitanju je treci tip marc-a, stavlja ga u state parsera
+    "LEADER" -> putState 3 -- drugi tip, ditto
     "LDR" -> putState 1 -- prvi, ditto
   spaces -- jede space-ove, ukoliko ih ima
   value <- many(noneOf [carriageReturn, newLine]) -- parsira sve karaktere dok ne dodje do CRLF karaktera
@@ -86,21 +82,21 @@ parseLeader = do
 parseFieldType1 :: Parsec String Int Field
 parseFieldType1 = do
   code <- count 3 digit -- parsira prve tri cifre, one predstasvljaju kod field-a
-  spaces -- jede space-ove ukolikoh ih ima
+  spaces -- jede space-ove ukoliko ih ima
   if((read code:: Int) < 10) then do -- field je kontrolnog tipa (svi manji od 010 su kontrolni), (read code:: Int) pretvara string u int
     value <- many(noneOf [rowSeparator]) -- vrednost field-a je sve dok se ne dodje do RS karaktera
     char rowSeparator -- jede RS karakter
     return (Field code [] [(Subfield "" value)]) -- vraca se field sa svojim kodom, praznim indikatorima i vrednoscu koja je predstavljena kroz potpolje sa praznim kodom i parsiranom vrednoscu
   else do -- field nije kontrolni, parsira i indikatore
-    indicators <- parseIndicatorsType1 -- parsira indikatore
+    indicators <- parseIndicators -- parsira indikatore
     subfields <- many parseSubfieldType1 -- parsira subfield-ove
     char rowSeparator -- jede RS karakter koji se nalazi na kraju field-a da bi mogao uspesno da se parsira sledeci field ukoliko postoji
     return (Field code indicators subfields) -- vraca field sa svojim kodom, indikatorima i potpoljima
 
 
 -- funkcija za parsiranje indikatora
-parseIndicatorsType1 :: Parsec String Int [Indicator] -- slicno kao i prethodne funkcije, samo sto vraca listu indikatora
-parseIndicatorsType1 = do
+parseIndicators :: Parsec String Int [Indicator] -- slicno kao i prethodne funkcije, samo sto vraca listu indikatora
+parseIndicators = do
   firstIndicator <- anyChar -- parsira bilo koji karakter
   secondIndicator <- anyChar -- ditto
   return [(Indicator firstIndicator), (Indicator secondIndicator)]
@@ -112,5 +108,34 @@ parseSubfieldType1 = do
    spaces -- jede moguce space-ove
    char '$' -- jede oznaku za pocetak subfield-a
    subfieldName <- anyChar -- parsira karakter koji predstavlja naziv subfield-a
+   spaces -- jede moguce space-ove
    subfieldValue <- many(noneOf ['$', rowSeparator]) -- parsira vrednost subfield-a, jeduci sve karaktere dok ne dodje do sledece oznake za subfield ili do kraja field-a
-   return (Subfield [subfieldName] subfieldValue) -- vraca subfield sa imenom (string duzine 1 karaktera) i vrdnoscu
+   return (Subfield [subfieldName] subfieldValue) -- vraca subfield sa imenom (string duzine 1 karaktera) i vrednoscu
+
+
+-- FUNKCIJE ZA PARSIRANJE DRUGOG TIPA MARC FORMATA
+
+-- funkcija za parsiranje fieldova za tip 2
+parseFieldType2 :: Parsec String Int Field
+parseFieldType2 = do
+  code <- count 3 digit -- parsira prve tri cifre, one predstasvljaju kod field-a
+  spaces -- jede space-ove ukoliko ih ima
+  if((read code:: Int) < 10) then do -- field je kontrolnog tipa (svi manji od 010 su kontrolni), (read code:: Int) pretvara string u int
+   value <- many(noneOf [rowSeparator]) -- vrednost field-a je sve dok se ne dodje do RS karaktera
+   char rowSeparator -- jede RS karakter
+   return (Field code [] [(Subfield "" value)]) -- vraca se field sa svojim kodom, praznim indikatorima i vrednoscu koja je predstavljena kroz potpolje sa praznim kodom i parsiranom vrednoscu
+  else do -- field nije kontrolni, parsira i indikatore
+   indicators <- parseIndicators -- parsira indikatore
+   subfields <- many parseSubfieldType2 -- parsira subfield-ove
+   char rowSeparator -- jede RS karakter koji se nalazi na kraju field-a da bi mogao uspesno da se parsira sledeci field ukoliko postoji
+   return (Field code indicators subfields) -- vraca field sa svojim kodom, indikatorima i potpoljima
+
+-- funkcija za parsiranje subfield-ova
+parseSubfieldType2 :: Parsec String Int Subfield
+parseSubfieldType2 = do
+spaces -- jede moguce space-ove
+char '|' -- jede oznaku za pocetak subfield-a
+subfieldName <- anyChar -- parsira karakter koji predstavlja naziv subfield-a
+spaces -- jede moguce space-ove
+subfieldValue <- many(noneOf ['|', rowSeparator]) -- parsira vrednost subfield-a, jeduci sve karaktere dok ne dodje do sledece oznake za subfield ili do kraja field-a
+return (Subfield [subfieldName] subfieldValue) -- vraca subfield sa imenom (string duzine 1 karaktera) i vrednoscu
